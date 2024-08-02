a23cd671c4e895541061dea5a1ee2d32c18464d4
Michael Calvin <pika242@gmail.com>
2024-08-01 22:49:32 -0500

    Optimizations

--- main.asm
+++ main.asm
@@ -24,7 +24,7 @@ FLAME_BUFFER_SIZE equ (FLAME_BUFFER_WIDTH * FLAME_BUFFER_HEIGHT)
 SEED_PADDING equ 16
 
 DATA segment
-	flameBuffer db 64000 dup(?)
+	flameBuffer db FLAME_BUFFER_SIZE dup(?)
 INCLUDE data\palette.asm
 	paletteEnd db 0
 	rng dw 4242h
@@ -120,26 +120,21 @@ UpdateFire PROC
 	
 	; Load buffer size
 	; cx = width
-	; dx = height
 	mov cx, FLAME_BUFFER_WIDTH
-	mov dx, FLAME_BUFFER_HEIGHT
 	
 	; Prepare row pointers
 	; bx = writing row
 	; di = reading row
 	mov bx, OFFSET flameBuffer
-	mov di, bx
-	add di, cx
+	mov di, (OFFSET flameBuffer) + FLAME_BUFFER_WIDTH
 	
-	mov ax, dx
-	sub ax, 1
+	mov ax, FLAME_BUFFER_HEIGHT - 1
 UpdateFire_Loop:
 	; Update the current row
 	call UpdateFireRow
 	
 	; Continue to the next row
 	dec ax
-	cmp ax, 0
 	jne UpdateFire_Loop
 
 	; Seed the last fire row
@@ -154,25 +149,20 @@ UpdateFire ENDP
 AveragePixel MACRO
 	mov dl, 3
 	div dl
-	mov ah, 0
 ENDM
 
 
 
-; Subtracts 1 from the value in ax if ax >= 1
-DarkenPixel PROC
-	cmp ax, FIRE_PIXEL_RUN_LENGTH
-	jb DarkenPixel_SetToZero
+; Subtracts FIRE_PIXEL_RUN_LENGTH from the value in ax, clamping at zero
+DarkenPixel MACRO
+	local DarkenPixel_FinishDarken
 	
-	sub ax, FIRE_PIXEL_RUN_LENGTH
-	jmp DarkenPixel_SkipDarken
+	sub al, FIRE_PIXEL_RUN_LENGTH
+	jnc DarkenPixel_FinishDarken
+	mov al, 0
 	
-DarkenPixel_SetToZero:
-	mov ax, 0
-	
-DarkenPixel_SkipDarken:
-	ret
-DarkenPixel ENDP
+DarkenPixel_FinishDarken:
+ENDM
 
 
 
@@ -182,82 +172,83 @@ DarkenPixel ENDP
 ; di = offset of the first element in the reading row
 ; bx = offset of the first element in the writing row
 ; cx = fire width
-; ax, cx, and dx are preserved by this proc
+; ax and cx are preserved by this proc
 ; di and bx are incremented to the start of their next respective rows after this proc finishes
 UpdateFireRow PROC
 	push ax
 	push cx
-	push dx
 
+	mov si, cx
+	
 	; The first pixel is a special case
 	; Sum the pixel
-	mov ax, 0
 	mov dh, 0
-	mov dl, [ds:di]
-	add ax, dx
+	
+	mov cl, [ds:di]
 	inc di
-	mov dl, [ds:di]
+	mov ch, [ds:di]
+	inc di
+	
+	mov dl, cl
+	mov ax, dx
+	mov dl, ch
 	add ax, dx
-	dec di
 	
 	; Average pixel and darken
 	AveragePixel
-	call DarkenPixel
+	DarkenPixel
 	
 	; Write the pixel and move to the next
-	mov [ds:bx], ax
+	mov [ds:bx], al
 	inc bx
 	
 	
-	dec cx
+	dec si
+	dec si
 UpdateFireRow_Loop:
-	; Sum the pixel data
-	mov ax, 0
-	mov dh, 0
-	mov dl, [ds:di]
+	; Add the previous two pixels
+	mov dl, cl
+	mov ax, dx
+	mov dl, ch
 	add ax, dx
+	
+	; Replace the oldest pixel with a new one
+	mov cl, ch
+	mov ch, [ds:di]
 	inc di
-	mov dl, [ds:di]
+	
+	; Add the new pixel
+	mov dl, ch
 	add ax, dx
-	inc di
-	mov dl, [ds:di]
-	add ax, dx
-	dec di
 	
 	; Average pixel and darken
 	AveragePixel
-	call DarkenPixel
+	DarkenPixel
 	
 	; Write the pixel and move to the next
-	mov [ds:bx], ax
+	mov [ds:bx], al
 	inc bx
 
 	; Loop?
-	dec cx
-	cmp cx, 1
+	dec si
 	jne UpdateFireRow_Loop
 	
 	; The last pixel is also a special case
-	; Sum the pixel
-	mov ax, 0
-	mov dh, 0
-	mov dl, [ds:di]
+	; Add the previous two pixels
+	mov dl, cl
+	mov ax, dx
+	mov dl, ch
 	add ax, dx
-	inc di
-	mov dl, [ds:di]
-	add ax, dx
-	inc di
 	
 	; Average pixel and darken
 	AveragePixel
-	call DarkenPixel
+	DarkenPixel
 	
 	; Write the pixel and move to the next
-	mov [ds:bx], ax
+	mov [ds:bx], al
 	inc bx
 	
 
-	pop dx
 	pop cx
 	pop ax
 	ret
@@ -274,32 +265,32 @@ SeedFire PROC
 	sub cx, SEED_PADDING shr FIRE_PIXEL_SIZE_SHIFT
 	add bx, (SEED_PADDING shr FIRE_PIXEL_SIZE_SHIFT) / 2
 	
-	mov di, OFFSET rng
+	mov ax, rng
 SeedFire_Loop:
 	; Check RNG
 	call UpdateRNG
-	mov ax, [ds:di]
 	cmp ax, 08000h
 	jb SeedFire_Zero
 	
 	; Seed with 1
-	mov al, 0ffh
-	mov [ds:bx], al
+	mov dl, 0ffh
+	mov [ds:bx], dl
 	
 	jmp SeedFire_Continue
 	
 	; Seed with 0
 SeedFire_Zero:
-	mov al, 0
-	mov [ds:bx], al
+	mov dl, 0
+	mov [ds:bx], dl
 
 SeedFire_Continue:
 	inc bx
 	
 	; Loop until finished
 	dec cx
-	cmp cx, 0
 	jne SeedFire_Loop
+	
+	mov rng, ax
 
 	ret
 SeedFire ENDP
@@ -307,15 +298,11 @@ SeedFire ENDP
 
 
 ; Updates the RNG value using a 16-bit LFSR
-; Upon entry, the following is assumed:
-; ds = segment of rng
-; di = offset of rng
+; Before calling, the RNG value to update should be in ax
+; After calling, the new RNg value is in ax
 UpdateRNG PROC
-	push ax
 	push bx
 	push cx
-	
-	mov ax, [ds:di]
 	
 	mov bx, ax
 	and bx, 1
@@ -345,11 +332,9 @@ REPT 15
 ENDM
 	shr ax, 1
 	or ax, bx
-	mov [ds:di], ax
 	
 	pop cx
 	pop bx
-	pop ax
 
 	ret
 UpdateRNG ENDP
@@ -373,49 +358,48 @@ CopyFirePixels PROC
 	mov di, 0
 	mov bx, OFFSET flameBuffer
 
-	mov dx, 0
-	mov cx, FIRE_PIXEL_RUN_LENGTH
+	mov dx, FLAME_BUFFER_WIDTH
 CopyFirePixels_Loop:
-	push dx
-	mov dx, SEG flameBuffer
-	mov ds, dx
-	mov al, [ds:bx]
-	mov dx, 0A000h
-	mov ds, dx
-	pop dx
+	mov si, SEG flameBuffer
+	mov ds, si
+	mov cl, [ds:bx]
+	mov si, 0A000h
+	mov ds, si
 	
 	
-	; Repeat horizontally
+	; Repeat block
+	mov al, FIRE_PIXEL_RUN_LENGTH
+CopyFirePixels_RepeatBlockVertical:
 	mov ah, FIRE_PIXEL_RUN_LENGTH
 CopyFirePixels_RepeatBlockHorizontal:
-	mov [ds:di], al
+	mov [ds:di], cl
 	inc di
 	
 	dec ah
-	cmp ah, 0
 	jne CopyFirePixels_RepeatBlockHorizontal
 	
-	; Next pixel in the row
+	; Repeat vertically
+	add di, VGA_SCREEN_WIDTH - FIRE_PIXEL_RUN_LENGTH
+	dec al
+	jne CopyFirePixels_RepeatBlockVertical
+	
+	; Next pixel
 	inc bx
-	inc dx
-
-	; If we're not at the end of the row, continue normally
-	cmp dx, FLAME_BUFFER_WIDTH
-	jne CopyFirePixels_Loop
 	
-	mov dx, 0
+	; Is it in the same row?
+	dec dx
+	je CopyFirePixels_NextRow
 	
-	; If we're done with this block of rows, go to the next loop iteration
-	dec cx
-	cmp cx, 0
-	je CopyFirePixels_ContinueLoop
+	; If so, go back to the first pixel of the next block
+	sub di, VGA_SCREEN_WIDTH * FIRE_PIXEL_RUN_LENGTH - FIRE_PIXEL_RUN_LENGTH
+	jmp CopyFirePixels_ContinueLoop
 	
-	; Otherwise, continue repeating the row
-	sub bx, FLAME_BUFFER_WIDTH
-	jmp CopyFirePixels_Loop
+	; If not, go to the next row
+CopyFirePixels_NextRow:
+	mov dx, FLAME_BUFFER_WIDTH
+	sub di, VGA_SCREEN_WIDTH - FIRE_PIXEL_RUN_LENGTH
 
 CopyFirePixels_ContinueLoop:
-	mov cx, FIRE_PIXEL_RUN_LENGTH
 	cmp bx, FLAME_BUFFER_SIZE
 	jb CopyFirePixels_Loop
 	
